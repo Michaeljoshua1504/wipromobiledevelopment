2617f916d176198acbb2f250f5b06708849de22a
git config --global user.name Michaeljoshua1504 <michaeljoshua.p1504@gmail.com>
2024-06-24 22:04:03 +0530

    Initial commit with all folders and files

---
+++ Day-7 Assignment/factorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+        int result = 1;
+
+        // range (num , 2)
+        for (int i = num; i > 1; i--) {
+            result *= i;
+        }
+
+        System.out.println("Factorial of " + num + ": " + result);
+        sc.close();
+    }
+}
